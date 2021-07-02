e4c29fbad2d813d85496c6f2233c1c1b34318747
Panic <panic.milos99@gmail.com>
2021-07-02 22:25:46 +0200

    feat: added newest data.csv. implemented some validation for csv. refactor: refactored converter from gs to yolo csv.

--- main.py
+++ main.py
@@ -9,36 +9,44 @@ def google_sheet_csv_to_yolo_csv(input_file_path=INPUT_FILE_PATH, output_file_pa
         lines = input_file_stream.readlines()
 
     with open(output_file_path, 'w') as output_file_stream:
-
         for line in lines:
-            # TODO: validate each line.
+            values = google_sheet_csv_line_to_yolo_values(line)
+            image, rest = values[0], values[1:]
 
-            line = line.replace(',', ' ').replace('"', '').strip()
-            image = line.split(' ')[0]
             output_file_stream.write(image_root_dir + image + '.jpg ')
-
-            rest = line.split(' ')[1:]
-            if len(rest) % 5 != 0:
-                print(image)
 
             num_of_bounding_boxes = int(len(rest) / 5)
             for i in range(num_of_bounding_boxes):
-                output_values = [
-                        rest[i*5],
-                        rest[i*5 + 1],
-                        str(int(rest[i*5]) + int(rest[i*5+2])),
-                        str(int(rest[i*5+1]) + int(rest[i*5+3])), rest[i*5+4]
-                    ]
-
-                output_file_stream.write(','.join(output_values))
+                output_file_stream.write(','.join(rest[i*5: i*5+5]))
                 if i < num_of_bounding_boxes - 1:
                     output_file_stream.write(" ")
 
             output_file_stream.write('\n')
 
 
+def google_sheet_csv_line_to_yolo_values(line):
+    values = line.replace(',', ' ').replace('"', '').strip().split(' ')
+
+    if len(values) % 5 != 1:  # one because of image on 0.
+        print('Not enough values on line:', line)
+
+    for i in range(1, len(values)):
+        if i % 5 in [3, 4]:  # adding top_x + width / top_y + height
+            values[i] = str(int(values[i-2]) + int(values[i]))
+
+        if i % 5 in [1, 2, 3, 4]:
+            if int(values[i]) < 0 or int(values[i]) > 800:
+                print('Coordinates must be in range [0, 800]. (', line.strip(), ')')
+
+        if i % 5 in [0]:
+            if values[i] not in ['0', '1']:
+                print('Class must be \'0\' or \'1\'. (', line.strip(), ')')
+
+    return values
+
+
 if __name__ == "__main__":
-    print("RECS1.6 Projekat")
+    print("RECS1.6 Projekat by ~Serbedzija, ~Panic")
 
     google_sheet_csv_to_yolo_csv()
 
